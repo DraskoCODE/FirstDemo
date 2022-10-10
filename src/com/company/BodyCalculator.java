4065571ed39b7ae91256d1c7f204ce7642c5f7ab
Marija <mabozz@live.com>
2022-10-10 18:38:38 +0200

    Implement BodyCalculator class

---
+++ src/com/company/BodyCalculator.java
@@ -0,0 +1,38 @@
+package com.company;
+
+public class BodyCalculator {
+    private double mass;
+    private double height;
+
+    public double getMass() {
+        return mass;
+    }
+
+    public void setMass(double mass) {
+        this.mass = mass;
+    }
+
+    public double getHeight() {
+        return height;
+    }
+
+    public void setHeight(double height) {
+        this.height = height;
+    }
+
+    public BodyCalculator (double mass, double height){
+        this.mass = mass;
+        this.height = height;
+    }
+
+    public double calculateBodyMassIndex (double mass, double height) {
+        double toReturn = 0;
+        toReturn = mass / (height * height);
+        return toReturn;
+
+    }
+
+
+
+
+}
--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,5 +4,9 @@ public class Main {
 
     public static void main(String[] args) {
 	 System.out.println("First GIT Project");
+
+     BodyCalculator bodyCalculator = new BodyCalculator(80,186);
+     System.out.println(bodyCalculator.calculateBodyMassIndex(bodyCalculator.getMass(), bodyCalculator.getHeight()));
+
     }
 }
